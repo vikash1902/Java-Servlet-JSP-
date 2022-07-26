0e8aed7407e8d5f0ce216c40481c7e95b7b63d94
VIKASH KUMAR MISHRA <87639872+vikash1902@users.noreply.github.com>
2022-07-26 12:18:59 +0530

    Create Servlet1.java

---
+++ ServletTutorial/Session/Hidden Form Field in Servlet/MyServlets/Servlet1.java
@@ -0,0 +1,66 @@
+
+package MyServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Servlet1 extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+         
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            // Fetch name from jsp
+            
+            String name = request.getParameter("user_name");
+            out.println("<h1>Your Name is: "+name+"</h1>");
+        //  out.println("<a href='Servlet2?user="+name+"'>Go To Servlet2</a>");
+            // using hidden form field 
+                
+             out.println(""
+                     + "<form action='Servlet2'>"
+                     + "<input type='hidden' name = 'user_name' value = '"+name+"'/> "
+                     + "<button>Go to second Servlet</button>"
+                     + ""
+                     + "</form>");
+                    
+          
+           
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
